348cb21729d37b07c32fc55c17f64f2a3128bf9d
George Roumieh <george95.2621@gmail.com>
2022-05-04 10:54:18 +0200

    deploy golang api

---
+++ application.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync/atomic"
+
+	"github.com/gorilla/mux"
+)
+type Message struct {
+	Msg string `json:"message"`
+}
+
+var called uint64
+var msg = Message{Msg:"Hello world"}
+
+
+func showMessage(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-type","application/json")
+	if r.URL.Path != "/" {
+		http.Error(w,"404 not found",http.StatusNotFound)
+		return
+	} 
+
+    if err := json.NewEncoder(w).Encode(msg.Msg); err != nil {
+        fmt.Println(err)
+        http.Error(w, "Error encoding response object", http.StatusInternalServerError)
+	}
+		json.NewEncoder(w).Encode(called)
+		atomic.AddUint64(&called, 1)
+	}
+
+	func updateMessage(w http.ResponseWriter,r *http.Request){
+		w.Header().Set("Content-type","application/json")
+		if r.URL.Path != "/" {
+			http.Error(w,"404 not found",http.StatusNotFound)
+			return
+		} 
+	
+		if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
+			fmt.Println(err)
+			http.Error(w, "Error decoidng response object", http.StatusBadRequest)
+			return
+		}
+		
+		if err := json.NewEncoder(w).Encode(msg.Msg); err != nil {
+			fmt.Println(err)
+			http.Error(w, "Error encoding response object", http.StatusInternalServerError)
+		}
+	}
+
+func main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+	port = "5000"
+    }
+
+	r := mux.NewRouter()
+	r.HandleFunc("/",showMessage).Methods("GET")
+	r.HandleFunc("/",updateMessage).Methods("PUT")
+
+    f, _ := os.Create("/var/log/golang/golang-server.log")
+    defer f.Close()
+    log.SetOutput(f)
+
+    fmt.Printf("Starting server at port :"+port)
+	http.ListenAndServe(":"+port,r)
+}
