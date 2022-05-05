a2d186d25ce63103403fa1649d80265063753925
George Roumieh <george95.2621@gmail.com>
2022-05-05 12:20:59 +0200

    add ignore file

--- application.go
+++ application.go
@@ -10,66 +10,64 @@ import (
 
 	"github.com/gorilla/mux"
 )
+
 type Message struct {
 	Msg string `json:"message"`
 }
 
 var called uint64
-var msg = Message{Msg:"Hello world"}
+var msg = Message{Msg: "Hello world"}
 
-
-func showMessage(w http.ResponseWriter,r *http.Request){
-	w.Header().Set("Content-type","application/json")
+func showMessage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
 	if r.URL.Path != "/" {
-		http.Error(w,"404 not found",http.StatusNotFound)
+		http.Error(w, "404 not found", http.StatusNotFound)
 		return
-	} 
-
-    if err := json.NewEncoder(w).Encode(msg.Msg); err != nil {
-        fmt.Println(err)
-        http.Error(w, "Error encoding response object", http.StatusInternalServerError)
-	}
-		json.NewEncoder(w).Encode(called)
-		atomic.AddUint64(&called, 1)
 	}
 
-	func updateMessage(w http.ResponseWriter,r *http.Request){
-		w.Header().Set("Content-type","application/json")
-		if r.URL.Path != "/" {
-			http.Error(w,"404 not found",http.StatusNotFound)
-			return
-		} 
-	
-		if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
-			fmt.Println(err)
-			http.Error(w, "Error decoidng response object", http.StatusBadRequest)
-			return
-		}
-		
-		if err := json.NewEncoder(w).Encode(msg.Msg); err != nil {
-			fmt.Println(err)
-			http.Error(w, "Error encoding response object", http.StatusInternalServerError)
-		}
+	if err := json.NewEncoder(w).Encode(msg.Msg); err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error encoding response object", http.StatusInternalServerError)
 	}
+	json.NewEncoder(w).Encode(called)
+	atomic.AddUint64(&called, 1)
+}
+
+func updateMessage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error decoidng response object", http.StatusBadRequest)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(msg.Msg); err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error encoding response object", http.StatusInternalServerError)
+	}
+}
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-	port = "5000"
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
 
 	r := mux.NewRouter()
-	r.HandleFunc("/",showMessage).Methods("GET")
-	r.HandleFunc("/",updateMessage).Methods("PUT")
+	r.HandleFunc("/", showMessage).Methods("GET")
+	r.HandleFunc("/", updateMessage).Methods("PUT")
 
-    f, _ := os.Create("/var/log/golang/golang-server.log")
-    defer f.Close()
-    log.SetOutput(f)
+	f, _ := os.Create("/var/log/golang/golang-server.log")
+	defer f.Close()
+	log.SetOutput(f)
 
-	// http.ListenAndServe(":"+port,r) 
-
-    fmt.Printf("Starting server at port :"+port)
-	err :=http.ListenAndServeTLS(":"+port,"localhost.crt","localhost.key",r) 
+	fmt.Printf("Starting server at port :" + port)
+	err := http.ListenAndServeTLS(":"+port, "localhost.crt", "localhost.key", r)
 	if err != nil {
 		log.Fatal(err)
 	}
